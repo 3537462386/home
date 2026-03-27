e2f825fb37067f2ff2a06ca0635b9258fb4bd3d0
卢雯 <3537462386@qq.com>
2026-03-27 17:25:03 +0800

    🐛 fix: 修复eslint配置格式，移除package-lock中的peer属性

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxt/content', '@nuxt/eslint', '@unocss/nuxt'],
-  eslint: {
-    config: {
-      stylistic: true,
-    },
-  },
   devtools: { enabled: true },
   compatibilityDate: '2024-04-03',
+  eslint: {
+    config: {
+      stylistic: true
+    }
+  }
 })
